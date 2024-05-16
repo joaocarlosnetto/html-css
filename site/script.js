5e9b3a081cd1e038b1011824217dce697eb43f80
joão carlos de caravlho <140458265+joaocarlosnetto@users.noreply.github.com>
2024-05-16 18:38:00 -0300

    atualizei meu repositorio

---
+++ site/script.js
@@ -0,0 +1,111 @@
+function rose(){
+    var img = document.getElementById('rose')
+    img.style.background = 'rgb(255, 149, 167)'
+    img.style.padding = '5px 27.7% 5px 27.7%'
+    img.style.borderRadius = '10px'
+    img.style.transition = '0.4s'
+    var tx = document.getElementById('txtr')
+    tx.style.display = 'block'
+    tx.style.transition = '0.4s'
+    tx.style.width = '500px'
+    tx.style.margin = 'auto'
+    tx.style.marginTop = '15px'
+    tx.style.fontSize = '1em'
+    var soo = document.getElementById('jisoo')
+    var nini = document.getElementById('jennie')
+    var lili = document.getElementById('lisa')
+    soo.style.padding = '0px'
+    nini.style.padding = '0px'
+    lili.style.padding = '0px'
+    var txs = document.getElementById('txts')
+    var txj = document.getElementById('txtj')
+    var txl = document.getElementById('txtl')
+    txs.style.display = 'none'
+    txj.style.display = 'none'
+    txl.style.display = 'none'
+}
+
+function jisoo(){
+    var img = document.getElementById('jisoo')
+    img.style.background = 'rgb(255, 149, 167)'
+    img.style.padding = '5px 27.7% 5px 27.7%'
+    img.style.borderRadius = '10px'
+    img.style.transition = '0.4s'
+    var tx = document.getElementById('txts')
+    tx.style.display = 'block'
+    tx.style.transition = '0.4s'
+    tx.style.width = '500px'
+    tx.style.margin = 'auto'
+    tx.style.marginTop = '15px'
+    tx.style.fontSize = '1em'
+    var rosie = document.getElementById('rose')
+    var nini = document.getElementById('jennie')
+    var lili = document.getElementById('lisa')
+    rosie.style.padding = '0px'
+    nini.style.padding = '0px'
+    lili.style.padding = '0px'
+    var txr = document.getElementById('txtr')
+    var txj = document.getElementById('txtj')
+    var txl = document.getElementById('txtl')
+    txr.style.display = 'none'
+    txj.style.display = 'none'
+    txl.style.display = 'none'
+}
+
+function jennie(){
+    var img = document.getElementById('jennie')
+    img.style.background = 'rgb(255, 149, 167)'
+    img.style.padding = '5px 27.7% 5px 27.7%'
+    img.style.borderRadius = '10px'
+    img.style.transition = '0.4s'
+    var tx = document.getElementById('txtj')
+    tx.style.display = 'block'
+    tx.style.transition = '0.4s'
+    tx.style.width = '500px'
+    tx.style.margin = 'auto'
+    tx.style.marginTop = '15px'
+    tx.style.fontSize = '1em'
+    var rosie = document.getElementById('rose')
+    var soo = document.getElementById('jisoo')
+    var lili = document.getElementById('lisa')
+    rosie.style.padding = '0px'
+    soo.style.padding = '0px'
+    lili.style.padding = '0px'
+    var txr = document.getElementById('txtr')
+    var txs = document.getElementById('txts')
+    var txl = document.getElementById('txtl')
+    txr.style.display = 'none'
+    txs.style.display = 'none'
+    txl.style.display = 'none'
+}
+
+function lisa(){
+    var img = document.getElementById('lisa')
+    img.style.background = 'rgb(255, 149, 167)'
+    img.style.padding = '5px 27.7% 5px 27.7%'
+    img.style.borderRadius = '10px'
+    img.style.transition = '0.4s'
+    var tx = document.getElementById('txtl')
+    tx.style.display = 'block'
+    tx.style.transition = '0.4s'
+    tx.style.width = '500px'
+    tx.style.margin = 'auto'
+    tx.style.marginTop = '15px'
+    tx.style.fontSize = '1em'
+    var rosie = document.getElementById('rose')
+    var soo = document.getElementById('jisoo')
+    var nini = document.getElementById('jennie')
+    rosie.style.padding = '0px'
+    soo.style.padding = '0px'
+    nini.style.padding = '0px'
+    var txr = document.getElementById('txtr')
+    var txs = document.getElementById('txts')
+    var txj = document.getElementById('txtj')
+    txr.style.display = 'none'
+    txs.style.display = 'none'
+    txj.style.display = 'none'
+}
+
+
+
+
